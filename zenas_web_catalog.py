d0bdf0373e1007f91e1b6c4c9e93bb1141f257f3
gigen4 <130969098+gigen4@users.noreply.github.com>
2023-05-15 18:22:18 +0200

    Update zenas_web_catalog.py

--- zenas_web_catalog.py
+++ zenas_web_catalog.py
@@ -9,8 +9,7 @@ my_cur = my_cnx.cursor()
 
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select color_or_style from ZENAS_ATHLEISURE_DB.PRODUCTS.catalog_for_website")
-## my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
-## my_catalog = my_cur.fetchall()
+my_catalog = my_cur.fetchall()
 
 # put the dafta into a dataframe
 ## df = pandas.DataFrame(my_catalog)

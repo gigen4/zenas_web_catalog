b2bd1fa4fe87050e5fb767c8ec2e0af806b63382
gigen4 <130969098+gigen4@users.noreply.github.com>
2023-05-17 01:32:15 +0200

    Update zenas_web_catalog.py

--- zenas_web_catalog.py
+++ zenas_web_catalog.py
@@ -29,7 +29,7 @@ product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
 # use the option selected to go back and get all the info from the database
 my_cur.execute("select direct_url, price, size_list, upsell_product_desc from ZENAS_ATHLEISURE_DB.PRODUCTS.catalog_for_website where color_or_style = '" + option + "';")
-## df2 = my_cur.fetchone()
+df2 = my_cur.fetchone()
 ## streamlit.image(
 ## df2[0],
 ## width=400,
